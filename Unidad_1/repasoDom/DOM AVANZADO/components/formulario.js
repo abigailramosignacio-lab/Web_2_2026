73998f5eec2b76b4e9721cb5d96eb0749e508ddb
abigailramosignacio-lab <abigailramosignacio@gmail.com>
2026-03-16 10:37:07 -0400

    cambios

---
+++ Unidad_1/repasoDom/DOM AVANZADO/components/formulario.js
@@ -0,0 +1,32 @@
+const Form = (()=>{
+      const form = document.querySelector('[data-form]');// accedemos al formulario
+      const inputTask = document.querySelector('[data-input-task]');// recupero input nombre de tarea
+      const inputDescription = document.querySelector('[data-input-descripcion]');//inport descripcion
+      const inputFecha = document.querySelector('[data-input-fecha]');//importacion fecha
+      const inputPrioridad = document.querySelector('[data-input-prioridad]');//inport prioridad
+
+      const datosForm = () =>{
+        return{
+                task: inputTask.value.trim(),
+                description: inputDescription.value.trim(),
+                date: inputFecha.value.trim(),
+                priority: inputPrioridad.value.trim()
+        };
+      };
+      const reset =()=>{
+        inputTask.value="";
+        inputDescription.value="";
+        inputFecha.value="";
+        inputPrioridad.value="";
+      }
+      const setDatos = (callback)=>{
+        form.addEventListener('submit',(event)=>{
+            event.preventDefault();
+            callback(datosForm());
+            reset();
+        });
+      };
+      return{setDatos,}
+       })();
+       export default Form;
+
---
+++ Unidad_1/repasoDom/DOM AVANZADO/components/tabla.js
@@ -0,0 +1,60 @@
+import cards from "./cards.js";
+const tabla = (()=>{
+           const cuerpoTabla = document.getElementById('taskTable').getElementsByTagName('tbody')[0];
+           const addTask = (task) => {
+               const nuevaFila = cuerpoTabla.insertRow();//creamos una nueva fila
+               nuevaFila.insertCell(0).textContent = task.task;
+               nuevaFila.insertCell(1).textContent = task.description;
+               nuevaFila.insertCell(2).textContent = task.date;
+               nuevaFila.insertCell(3).textContent =task.priority;
+               //agregar acciones
+               const accionCell = nuevaFila.insertCell(4);
+               const acciones = document.createElement('div');
+               acciones.className='actions';
+
+               //crear Botones
+               //HECHO
+               const completeButton = document.createElement('button');
+               completeButton.textContent='Hecho';
+               completeButton.className='view';
+               completeButton.addEventListener('click',()=>{
+                  nuevaFila.classList.toggle('completed');
+                  cards.update();
+                  //pendiente actualizar cards
+               });
+               acciones.appendChild(completeButton);
+
+               //ELIMINAR
+               const deleteButton = document.createElement('button');
+               deleteButton.textContent='eliminar';
+               deleteButton.className = 'delete';
+               deleteButton.addEventListener('click',()=>{
+                   cuerpoTabla.deleteRow(nuevaFila.rowIndex-1);
+                   cards.update(); 
+                   //pendienete actualizar cards
+               });
+               acciones.appendChild(deleteButton);
+               accionCell.appendChild(acciones);
+
+               //EDITAR
+               const editButton = document.createElement('button');
+               editButton.textContent = 'Editar';
+               editButton.className='edit';
+               editButton.addEventListener('click', ()=>{
+                ///////
+               })
+           };
+           const getTask = ()=>{
+                return Array.from(cuerpoTabla.rows).map(row =>({
+                    task:row.cells[0].textContent,
+                    description: row.cells[1].textContent,
+                    date:row.cells[2].textContent,
+                    priority:row.cells[3].textContent,
+                    completed:row.classList.contains('completed')
+                }));
+           };
+           return{addTask,getTask}          
+       })();
+       export default tabla;
+
+
